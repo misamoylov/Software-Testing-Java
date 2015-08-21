b053db5c017261eff12c644e75d704f391ead71c
misamoylov <mikhailsamoiloff@gmail.com>
2015-08-21 23:51:21 +0300

    Lesson1

---
+++ address-book-test1/src/addressbook/tests/EmptyGroupCreation.java
@@ -0,0 +1,17 @@
+package addressbook.tests;
+
+
+import org.testng.annotations.Test;
+
+public class EmptyGroupCreation extends TestBase {
+  @Test
+  public void testNonEmptyGroupCreation() throws Exception {
+	openMainPage();
+	gotoGroups();
+    groupCreation();
+	fillGroupForm(new GroupData("","",""));
+    submitGroupCreation();
+    returnToGroupPage();
+  }
+
+}
---
+++ address-book-test1/src/addressbook/tests/NonEmptyGroupCreation.java
@@ -0,0 +1,19 @@
+package addressbook.tests;
+
+import org.testng.annotations.Test;
+
+public class NonEmptyGroupCreation extends TestBase {
+  @Test
+  public void testNonEmptyGroupCreation() throws Exception {
+	openMainPage();
+	gotoGroups();
+    groupCreation();
+    GroupData group = new GroupData();
+    group.name = "group name 1";
+    group.header = "header name 1";
+    group.footer = "footer variable 1";
+	fillGroupForm(group);
+    submitGroupCreation();
+    returnToGroupPage();
+  }
+}
